aeea8df6340c6fd0d4874de9cffccabe9c187bd0
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-10-24 20:24:48 +0100

    Set MoviesContainer as the root component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Movies from './containers/movies/movies';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Movies />, document.getElementById('root'));
 registerServiceWorker();
