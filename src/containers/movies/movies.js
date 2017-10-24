c08e2f40341985c51af1a5589364ce4c3a2e7559
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-10-24 22:03:35 +0100

    Update movies container to show one MovieItem per each item on the mockdata

--- src/containers/movies/movies.js
+++ src/containers/movies/movies.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import './movies.scss';
+import './movies.css';
+
+import MoviesData from './films.json';
 
 import { MovieItem } from '../../components/movieItem/movieItem';
 
 class Movies extends Component {
   render() {
     return (
-      <div>
-        Movies Container
-        <MovieItem title="Interestellar" />
+      <div className="moviesContainer">
+        {
+          MoviesData.data.allMovies.map((movie, i) =>
+            <MovieItem 
+              key={i}
+              title={movie.title} 
+              poster_image={movie.poster_image}
+              rating={movie.rating}
+            />
+          ) 
+        }
       </div>
     );
   }
 }
 
 export default Movies;
+
+
+/* 
+// SOLUCIÓN
+
+import React from 'react';
+
+export const MovieItem = props =>
+  <div>
+    <p> Titulo:  {props.title} </p>
+  </div>
+*/
