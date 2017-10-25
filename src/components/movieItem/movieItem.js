4098fa1ad3304b1d31cf9a12e06ce7503b5d6f65
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-10-25 19:03:14 +0100

    Update movieItem component

--- src/components/movieItem/movieItem.js
+++ src/components/movieItem/movieItem.js
@@ -1,26 +1,9 @@
-/* 
-  Ejercicio 1. Refactoriza el componente para mostrar:
-  - El titulo
-  - La imagen
-  - El rating
-
-  Has de utilizar las classes movie para el contenedor y la clase posterImage para la imagen
-*/
-
 import React from 'react';
 import './movieItem.css';
 
 export const MovieItem = props =>
-  <div>
-    <p> {props.title} </p>
-  </div>
-
-/* 
-  // SOLUCIÓN
-
   <div className="movie">
     <img src={props.poster_image}  className="posterImage" />
     <h4> {props.title} </h4>
     <p> Rating:  {props.rating} </p>
   </div>
-*/
