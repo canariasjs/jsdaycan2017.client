3778c4f83eb649ac34015a952462140f612a925c
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-11-06 14:37:24 +0000

    Update queries by adding moviesById

--- src/queries/movies.js
+++ src/queries/movies.js
@@ -12,3 +12,28 @@ export const allMovies = gql`
     }
   }
 `
+
+/* Ejercicio: Indica a GraphQL que campos debe devolverte cuando pidas los detalles de una película */
+
+export const movieById = gql`
+  query moviesById($id: [Int]!) {
+    moviesById(id: $id) {
+    }
+  }
+`
+
+
+/*
+export const movieById = gql`
+  query moviesById($id: [Int]!) {
+    moviesById(id: $id) {
+      id
+      title,
+      description,
+      poster_image,
+      year,
+      rating
+    }
+  }
+`
+*/
