0fd41fdf0068c8fd6ae9cf8ef53ee1aa50d05c7e
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-11-06 14:04:08 +0000

    Update index.js by adding Apollo HOC

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import './index.css';
 import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// GraphQL client
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:4500/graphql'
+})
+const client = new ApolloClient({
+  networkInterface
+})
+
+
 ReactDOM.render(
   <BrowserRouter>
-    <Router />
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   </BrowserRouter>
   , document.getElementById('root')
 );
