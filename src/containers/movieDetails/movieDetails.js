eb9bb8af8661e5f7cb51c646ad13ddbdff1f13ad
ivanbtrujillo <ivanbtrujillo@gmail.com>
2017-11-06 12:08:13 +0000

    Add button into movies container to create new movie

--- src/containers/movieDetails/movieDetails.js
+++ src/containers/movieDetails/movieDetails.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import './movieDetails.css';
 
-/* 
-  Ejercicio 1. En este componente recibimos a través del router la película que queremos
-  ver en props.location.state.
-  Has de asociar este state recibido al state del componente. 
-*/
-
-/* Ejercicio 2.
-  Muestra en el componente el titulo (h4) y la descripción de la película (<p>)
-*/
-
 class MovieDetails extends Component {
   constructor(props){
     super(props);
-    // this.state = props.location.state;
+    this.state = props.location.state;
   }
 
   render() {
     return (
       <div className="movieDetails">     
-        {/* <h4> {this.state.movie.title} </h4>
-        <p> {this.state.movie.description} </p>  */}
+        <img src={this.state.movie.poster_image} width="80px" height="auto" />
+        <h4> {this.state.movie.title} </h4>
+        <p> {this.state.movie.description} </p> 
       </div>
     );
   }
--- src/containers/movies/movies.js
+++ src/containers/movies/movies.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import './movies.css';
 
 import MoviesData from './films.json';
@@ -7,35 +8,37 @@ import MoviesData from './films.json';
 import { MovieItem } from '../../components/movieItem/movieItem';
 
 class Movies extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      showModal: false,
+    }
+  }
+
+  open = () => this.setState({ showModal: true });
+  close = () => this.setState({ showModal: false });
+
   render() {
+
     return (
-      <div className="moviesContainer">
-        {
-          MoviesData.data.allMovies.map((movie, i) => 
-          <Link  key={i} to={{ pathname: `/movies/${movie.id}`, state: { movie } }}>
-            <MovieItem 
-              title={movie.title} 
-              poster_image={movie.poster_image}
-              rating={movie.rating}
-            />
-          </Link>
-          ) 
-        }
-      </div>
+
+        <div className="moviesContainer">
+          <Button bsStyle="primary" onClick={this.open}> New movie </Button>
+          {
+            MoviesData.data.allMovies.map((movie, i) => 
+            <Link  key={i} to={{ pathname: `/movies/${movie.id}`, state: { movie } }}>
+              <MovieItem 
+                title={movie.title} 
+                poster_image={movie.poster_image}
+                rating={movie.rating}
+              />
+            </Link>
+            ) 
+          }
+        </div>
     );
   }
 }
 
 export default Movies;
-
-
-/* 
-// SOLUCIÓN
-
-import React from 'react';
-
-export const MovieItem = props =>
-  <div>
-    <p> Titulo:  {props.title} </p>
-  </div>
-*/
